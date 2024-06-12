fc9cc91594864fabd66c5ba85a917f8699bc9a2b
Harsh-Portfolio <49harshyadav@gmail.com>
2024-06-12 12:53:07 +0530

    stack imp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-          Hello! I’m a passionate full-stock developer. My name is {` `}
+          Hello! I’m a passionate full-stack developer. My name is {` `}
             {/* Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `} */}
             <span className="inline-block relative">
               Harsh Yadav{" "}
